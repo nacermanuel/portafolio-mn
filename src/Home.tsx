b016ba80a1270dacad79705a60cdd654fe3fb9ea
Manuel <nacermanuel@gmail.com>
2023-06-05 08:35:26 -0500

    deleted mock button

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AppEn from './AppEn.tsx'
 import AppEs from './AppEs.tsx'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export const Home = () => {
     const [lang, setLang] = React.useState(true)
@@ -9,7 +9,7 @@ export const Home = () => {
     <>
         {lang ? <AppEn language={()=> setLang(!lang)}/> : <AppEs language={()=> setLang(!lang)}/>}
     
-        <Link to="./mock"><div className='bg-white text-slate-950 mt-11 w-fit text-sm'>mock click here</div></Link>
+        {/* <Link to="./mock"><div className='bg-white text-slate-950 mt-11 w-fit text-sm'>mock click here</div></Link> */}
       
     </>
 
