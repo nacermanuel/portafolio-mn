282cfd603a57efc43bf9c473fe607bf90378baea
Manuel <nacermanuel@gmail.com>
2023-06-08 10:36:32 -0500

    minor change

--- src/AppEn.tsx
+++ src/AppEn.tsx
@@ -97,7 +97,7 @@ function AppEn({language}: Prop) {
 
       {/* SECCION 2 */}
       <div id="divSec2" className="bg-[#302b46] w-full rounded-lg py-4 px-12 font-semibold text-center hover:bg-[#574e80] cursor-none max-[1024px]:w-[95%] max-[1024px]:px-2">
-        Hello, I'm Manuel, a full-stack web developer. I've acquired  my skills through a rigorous bootcamp called SoyHenry and extensive self-study, but my true expertise has come from building my own projects. My passion lies in leveraging technology as a problem solver, and my goal is to work within a company that shares this vision.
+        Hello, I'm Manuel, a full-stack web developer. I've acquired  my skills through a rigorous bootcamp called SoyHenry and extensive self-study, but my true expertise has come from building my own projects. My passion lies in leveraging technology as a problem solver, and my goal is to work on projects that align with this vision.
         <br />
         <br />
          In addition to my technical skills, I have held leading roles in the operation of businesses with annual revenue surpassing USD$100,000. This hands-on experience has given me a deep understanding of commercial strategies, expertise in running complex Facebook ads campaigns, and a comprehensive grasp of financial structures within businesses. I leverage my tech skills and business expertise to bring value to the table. 
